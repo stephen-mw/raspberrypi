c7107a91ed026c9588f8014b90e442aa8a14a4d1
Stephen Wood <swood@netflix.com>
2022-12-11 15:52:02 -0700

    Update configurations.

---
+++ configuration/assets/profile.sh
@@ -0,0 +1,36 @@
+# make less case insensitive for searches and display verbose prompt by default
+export LESS="-irMS"
+
+# Add terminal colors
+export CLICOLOR=1
+
+# Don't put duplicate lines in the history. also, don't save lines that begin
+# with a whitespace character
+export HISTCONTROL=erasedups:ignorespace
+export HISTTIMEFORMAT='%F %T '
+export HISTSIZE=20000
+shopt -s histappend
+
+# Make the bash history save for all open terminals
+export PROMPT_COMMAND='history -a'
+
+# Collapses multi-line commands into a single history item
+shopt -s cmdhist
+
+# ignore these short commands in the history
+export HISTIGNORE="&:cd:fg:bg:pwd:pd:po:pushd:popd:dirs:ls:jobs:top"
+
+alias grp="grep -rnI"
+
+# Better ls
+alias ll="ls -ltr"
+
+# Format python
+function lint {
+    if [[ -z "${1+x}" ]]; then
+        echo "First argument must be a python file."
+        return
+    fi
+    black --line-length=120 ${1}
+    isort --profile=black ${1}
+}
